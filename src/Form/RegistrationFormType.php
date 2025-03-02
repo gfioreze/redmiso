d8a3388dc557412a2a1b5cc1f57ecb46fc59d0ff
Gabriel <78756794+gfioreze@users.noreply.github.com>
2025-03-02 16:47:01 +0000

    dunno what I did dont wanna merge conflicts

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'required' => 'true',
+                'attr' => ['class' => 'register__input'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a name',
+                    ]),
+                ]
+            ])
+
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'register__input'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email'
+                    ]),
+                    new Email([
+                        'message' => 'Please enter a valid email address'
+                    ])
+                ]
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'I agree to the terms and conditions',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms and conditions',
+                    ]),
+                ],
+                'attr' => ['class' => 'register__checkbox']
+            ])
+
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'register__input'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
+
